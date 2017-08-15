30820f98ec3458a1964ad21048b00cd01f4ae0e6
Shane Moyo <smoyo@pdx.edu>
2017-08-15 13:34:15 -0700

    refactored code a bit

--- app.js
+++ app.js
@@ -3,10 +3,6 @@
 var allProducts = [];
 var imgQue = [];
 var display = document.getElementById("display");
-var parent = document.getElementById("display");
-var newProduct1;
-var newProduct2;
-var newProduct3;
 var clicks = 0;
 
 
@@ -19,11 +15,9 @@ function createProduct(img, name){
     this.name = name;
     allProducts.push(this); //Adds to product to array.
     console.log(this.name + " product has been created");
-    console.log(allProducts)
 };
 
 function reloadQue(){//Creates 6 unique random numbers and adds them to imgQue array. 
-  
   while(imgQue.length <  6 ){
   var num = Math.floor(Math.random() * allProducts.length );
   while(imgQue.includes(num)){
@@ -36,62 +30,50 @@ function reloadQue(){//Creates 6 unique random numbers and adds them to imgQue a
 //Function that will add 3 objects from allProducts array at indices corresponding to imgQue array to the DOM
 //Displaying their image property.
 function addToDOM(){
-  
-  newProduct1 = document.createElement("img");
-  newProduct2 = document.createElement("img");
-  newProduct3 = document.createElement("img");
+  var newProduct1 = document.createElement("img");
+  var newProduct2 = document.createElement("img");
+  var newProduct3 = document.createElement("img");
   newProduct1.setAttribute("src", allProducts[imgQue[0]].img);
   newProduct1.setAttribute("class", "product");
-  newProduct1.setAttribute("id", "product1");
+  newProduct1.setAttribute("id", allProducts[imgQue[0]].name);
   newProduct2.setAttribute("src", allProducts[imgQue[1]].img);
   newProduct2.setAttribute("class", "product");
-  newProduct2.setAttribute("id", "product2");
+  newProduct2.setAttribute("id", allProducts[imgQue[1]].name);
   newProduct3.setAttribute("src", allProducts[imgQue[2]].img);
   newProduct3.setAttribute("class", "product");
-  newProduct1.setAttribute("id", "product3");
+  newProduct3.setAttribute("id", allProducts[imgQue[2]].name);
   display.appendChild(newProduct1);
   display.appendChild(newProduct2);
   display.appendChild(newProduct3);
   for(i = 0; i < 3; i++ ){//Raises show prperty of displayed products by 1. 
     allProducts[imgQue[i]]['shown'] = allProducts[imgQue[i]].shown + 1;
   }
-  newProduct1.addEventListener("click", voted1);
-  newProduct2.addEventListener("click",voted2);
-  newProduct3.addEventListener("click", voted3);
+  display.addEventListener("click", vote);
   reloadQue();
-  console.log("imgQue a2d " +imgQue);
 }
 
-
-
-function voted1(){//Raises vote property of whatever object corresponds to newProduct1.
-  allProducts[imgQue[0]]['voted'] = allProducts[imgQue[0]].voted + 1;
+function vote(){//Raises vote property of whatever object corresponds to the image clicked.
+  var clicked = event.target
+  for (i = 0; i < allProducts.length; i ++){//finds object assciated with image
+    if(allProducts[i].name === clicked.id){//Once object is found voted property is increased by 1
+      allProducts[i]['voted'] = allProducts[i].voted +1
+      console.log(allProducts[i].name + " votes: " + allProducts[i].voted);
+    }
+  }
   nextProducts();
   clicks += 1;
-  console.log("clicks : " + clicks);
-}
-function voted2(){//Raises vote property of whatever object corresponds to newProduct2.
-  allProducts[imgQue[1]]['voted'] = allProducts[imgQue[1]].voted + 1;
-  nextProducts();
-  clicks += 1;
-   console.log("clicks : " + clicks);
-}
-function voted3(){//Raises vote property of whatever object corresponds to newProduct3.
-  allProducts[imgQue[2]]['voted'] = allProducts[imgQue[2]].voted + 1;
-  nextProducts();
-  clicks += 1;
-   console.log("clicks : " + clicks);
 }
 
 function nextProducts(){//Removes first three used indices stored in imgQue then removes the 3 products displayed. 
   imgQue.splice(0,3);
   for(i = 0; i < 3; i++ ){
     var child = document.getElementsByClassName("product")[0];
-    parent.removeChild(child);
+    display.removeChild(child);
   }
 addToDOM();
 };
 
+//Creating all object instances 
 var bag = new createProduct("img/bag.jpg", "bag");
 var banana = new createProduct("img/banana.jpg", "banana");
 var bathroom = new createProduct("img/bathroom.jpg", "bathroom");
