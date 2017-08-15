9b8aed9515cd1c078660c99213af87556a84cdae
Shane Moyo <smoyo@pdx.edu>
2017-08-15 15:08:54 -0700

    made a chart, displays when clicks reaches 25

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var imgQue = [];
 var display = document.getElementById("display");
 var clicks = 0;
 
-
-
 //Creates each prodcut instance. Adds to allProducts array.
 function createProduct(img, name){
     this.img = img;
@@ -60,8 +58,8 @@ function vote(){//Raises vote property of whatever object corresponds to the ima
       console.log(allProducts[i].name + " votes: " + allProducts[i].voted);
     }
   }
-  nextProducts();
   clicks += 1;
+  if(clicks < 25){nextProducts();} else { makeChart();} 
 }
 
 function nextProducts(){//Removes first three used indices stored in imgQue then removes the 3 products displayed. 
@@ -71,7 +69,9 @@ function nextProducts(){//Removes first three used indices stored in imgQue then
     display.removeChild(child);
   }
 addToDOM();
+console.log("clicks at: " + clicks);
 };
+
 
 //Creating all object instances 
 var bag = new createProduct("img/bag.jpg", "bag");
@@ -84,5 +84,47 @@ var chair = new createProduct("img/chair.jpg", "chair");
 var cthulhu = new createProduct("img/cthulhu.jpg", "cthulhu");
 var dogduck = new createProduct("img/dog-duck.jpg", "dog duck");
 var dragon = new createProduct("img/dragon.jpg", "dragon");
+var pen = new createProduct("img/pen.jpg", "pen");
+var petSweep = new createProduct("img/pet-sweep.jpg", "pet-sweep");
+var scissors = new createProduct("img/scissors.jpg", "scissors");
+var shark = new createProduct("img/shark.jpg", "shark");
+var sweep = new createProduct("img/sweep.png", "sweep");
+var tauntaun = new createProduct("img/tauntaun.jpg", "tauntaun");
+var unicorn = new createProduct("img/unicorn.jpg", "unicorn");
+var usb = new createProduct("img/usb.gif", "usb");
+var waterCan = new createProduct("img/water-can.jpg", "water-can");
+var wineGlass = new createProduct("img/wine-glass.jpg", "wineGlass");
 reloadQue();
 addToDOM();
+
+//Creates chart showing items vote total compared with the shown total. 
+function makeChart() {
+var chartCanvas = document.getElementById('graph');
+var myChart = new Chart (chartCanvas, {
+  type: 'bar',
+  data: {
+    labels: ['bag','banana','bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogduck', 'dragon',
+     'pen', 'pet sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water can', 'wine glass'],
+    datasets: [{
+      label: 'Votes',
+      data: [bag.voted, banana.voted, bathroom.voted, boots.voted, breakfast.voted, bubblegum.voted, chair.voted,
+      cthulhu.voted, dogduck.voted, dragon.voted, pen.voted, petSweep.voted, scissors.voted, shark.voted, sweep.voted,
+      tauntaun.voted, unicorn.voted, usb.voted, waterCan.voted, wineGlass.voted],
+      backgroundColor: 'rgb(10,181,61)'
+    },
+    {
+      label: 'shown',
+      data: [bag.shown, banana.shown, bathroom.shown, boots.shown, breakfast.shown, bubblegum.shown, chair.shown,
+      cthulhu.shown, dogduck.shown, dragon.shown, pen.shown, petSweep.shown, scissors.shown, shark.shown, sweep.shown,
+      tauntaun.shown, unicorn.shown, usb.shown, waterCan.shown, wineGlass.shown],
+      backgroundColor: 'rgba(10,118,181, 0.4)'
+    }]
+  },
+  options: {
+        title: {
+            display: true,
+            text: 'Vote Total'
+        }
+},
+});
+}
